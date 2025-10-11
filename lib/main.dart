45f08ba0f51f3717ae60801c3b4b29b34a5ebd49
kamrankhan67 <mr.kamrankhan67@gmail.com>
2025-10-12 03:41:31 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:socializer/features/auth/presentation/pages/login_page.dart';
+import 'package:socializer/features/auth/presentation/pages/signup_page.dart';
 import 'package:socializer/features/theme/light_theme.dart';
 import 'package:socializer/firebase_options.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: lightTheme,
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignupPage(),
     );
   }
 }
